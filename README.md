6df183e66614b280f082c74f9f25a1b3d4317e53
Dominik Welland <hallo@doik.de>
2015-09-11 20:55:57 +0200

    reset exhaustion when the player eats

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ spigot-teleport-plugin
 ===========
 
 This plugin increases the player's exhaustion by one every time the tppos command is used.
+If the exhaustion level exceeds 4 (see minecraft food mechanics), the hunger-llevel is increased, and the exhaustion level reset.
+If the player eats, the exhaustion level is reset as well.
--- src/com/micropsi/teleportfood/TeleportListener.java
+++ src/com/micropsi/teleportfood/TeleportListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class TeleportListener implements Listener {
 
@@ -22,9 +23,20 @@ public class TeleportListener implements Listener {
 				// max exhaustion level is 4. See minecraft hunger mechanics
 				if(player.getExhaustion() + 1 > 4){
 					player.setFoodLevel(player.getFoodLevel() - 1);
+					player.setExhaustion(0);
+				} else {
+					player.setExhaustion((float) (player.getExhaustion() + 1));
 				}
-				player.setExhaustion((float) (player.getExhaustion() + 1));
 			}
+		}
+	}
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event){
+		if(event.getItem().getType().isEdible()){
+			// player is eating, reset exhaustion level
+			Player player = event.getPlayer();
+			player.setExhaustion(0);
 		}
 	}
 
